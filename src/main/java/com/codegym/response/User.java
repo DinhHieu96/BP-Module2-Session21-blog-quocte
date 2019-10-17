6c77d5a48f7af3cf0370be2e63b598b8eb2149f0
DinhHieu96 <truongdinhhieu1496@gamil.com>
2019-10-17 21:08:17 +0700

    cm

---
+++ src/main/java/com/codegym/response/User.java
@@ -0,0 +1,20 @@
+package com.codegym.response;
+
+public class User {
+    private String userName;
+
+    public User() {
+    }
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
